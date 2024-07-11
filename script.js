4e715e298773d344894d7798e1b8c4e8e9b490a9
Varun Dhand <varundhand77@gmail.com>
2024-07-11 15:31:07 +0530

    Closures

--- script.js
+++ script.js
@@ -22,3 +22,31 @@ function outerFunction() {
 outerFunction();
 // innerFunction(); // Error: innerFunction is not defined
 
+//! Closure
+// Function + Lexical Environment = Closure
+// Closure is a function that has access to its outer function scope even after the outer function has finished has been called.
+// its used in module pattern, currying, and memoization.
+// example: 
+function outerFunction(outerVariable) {
+    return function innerFunction(innerVariable) {
+        console.log('Outer variable: ' + outerVariable);
+        console.log('Inner variable: ' + innerVariable);
+    };
+}
+
+const newFunction = outerFunction('outside');
+newFunction('inside');
+
+// example 2 with fetch:
+function outerFunction(url){
+    fetch(url) // the inner function has access to the url variable which is lexically scoped to the outer function
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+    })
+    .catch(error => {
+        console.log(error);
+    });
+}
+
+const newFunc = outerFunction('https://jsonplaceholder.typicode.com/posts/1'); 
